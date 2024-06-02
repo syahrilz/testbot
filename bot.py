ec5a03b7273222068d3e69743783e72967d2152e
Muhammad Syahril Syarif <103059057+syahrilz@users.noreply.github.com>
2024-06-02 10:33:32 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+token = os.getenv('DISCORD_TOKEN')
+client.run(token)
